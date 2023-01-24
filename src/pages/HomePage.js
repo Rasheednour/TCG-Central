ceb351d5e0f5959d45a6766269d6fcf70d567b4a
Rasheednour <rasheednour@gmail.com>
2023-01-25 00:38:53 +0100

    added home page file

---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+
+        <p>
+         This is the Home Page
+        </p>
+        
+    </div>
+  );
+}
+
+export default HomePage;
