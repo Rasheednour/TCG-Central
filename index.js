5863a3ed6bfcd384b074cf8663abc71a23bc6bae
Stew <weazle1@gmail.com>
2023-01-27 00:15:59 +0000

    basic get all games endpoint up and running

--- index.js
+++ index.js
@@ -12,3 +12,20 @@ app.listen(port, () => {
 app.get('/', async (req, res) => {
     res.json({status: 'tcgbackend is up and listening'});
 });
+
+app.get('/games', async (req, res) => {
+    console.log("games endpoint was hit");
+    let query = db.collection('games');
+    let snapshot = await query.get();
+    if (snapshot.empty){
+	console.log("no entries found for games");
+	res.json({});
+    }
+    console.log(`Found ${snapshot.size} games`);
+    let allGames = {};
+    snapshot.forEach(doc => {
+	allGames[doc.id] = doc.data();
+    });
+    res.json(allGames);
+});
+
