a95558bc268d4d4cdb7b8b6d6881c9a24afb8b09
Rasheednour <rasheednour@gmail.com>
2023-01-29 17:36:02 +0100

    Feat: created home page intro panel

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,18 @@
 import '../App.css';
 import TopRibbon from '../components/TopRibbon.js'
+import Button from '@mui/material/Button';
+
 
 function HomePage() {
   return (
     <div className="HomePage">
 
         <TopRibbon/>
-        <p>
-         This is the Home Page
-        </p>
+        <div className='home-intro'>
+          <h1>Community Created Trading Card Games</h1>
+          <p>TCG CENTRAL is your one stop shop to view, and play trading card games created by a community of thousands of creative individuals, as well as create your own.</p>
+          <Button variant="contained">VIEW PUBLISHED GAMES</Button>
+        </div>
         
     </div>
   );
