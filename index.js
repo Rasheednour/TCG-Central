b61bb5d410f51ee70d584bb54703e471f7bfcee6
Stew <weazle1@gmail.com>
2023-01-29 17:08:20 +0000

    basic create and lookup for game

--- index.js
+++ index.js
@@ -13,12 +13,14 @@ app.get('/', async (req, res) => {
     res.json({status: 'tcgbackend is up and listening'});
 });
 
+//GET all games - returns an object containing each game keyed to its game Id
 app.get('/games', async (req, res) => {
     console.log("games endpoint was hit");
     let query = db.collection('games');
     let snapshot = await query.get();
     if (snapshot.empty){
 	console.log("no entries found for games");
+	res.status(404);
 	res.json({'error': 'no games found'});
     }
     console.log(`Found ${snapshot.size} games`);
@@ -29,10 +31,57 @@ app.get('/games', async (req, res) => {
     res.json(allGames);
 });
 
+//POST Game - creates a new game, or if the game already exists overwrites that game
+app.post('/games', async (req, res) => {
+    let game = req.body;
+    if ('game_id' in game) {
+	let id = game['game_id'];
+	delete game.game_id;
+	let newGame = await db.collection('games').doc(id).set(game);
+	res.json({"game_id": id});
+    } else {
+	let newGame = await db.collection('games').add(game);
+	res.json({'game_id': newGame.id});
+    }
+});
+
+//GET 1 game - add game_id attribute
+app.get('/games/:gameId', async (req, res) => {
+    let query = db.collection('games').doc(req.params.gameId);
+    let doc = await query.get();
+    if (doc.exists()){
+	let game = doc.data();
+	game['game_id'] = doc.id;
+	res.json(game);
+    }
+    else {
+	res.status(404);
+	res.json({'error': 'game not found'});
+    }
+}):
+
+
+
+//GET 1 card (dunno why this would get used but put it in for completeness)
+app.get('/cards/:cardId', async (req, res) => {
+    let card_id = req.params.cardId;
+    let query = db.collection('cards').doc(card_id);
+    let doc = await query.get();
+    if (card.exists){
+	let card = doc.data();
+	card['card_id'] = doc.id;
+	res.json(card);
+    }
+    else {
+	res.status(404);
+	res.json({'error': 'card not found'})
+    }
+});
+
 //GET All cards for a given game
 app.get('/games/:gameId/cards', async (req, res) => {
     let gameId = req.params.gameId;
-    let query = db.collection('cards').where("game_ids", "array_contains", gameId);
+    let query = db.collection('cards').where("game_ids", "array-contains", gameId);
     let snapshot = await query.get();
     let allCards = {};
     snapshot.forEach(doc => {
@@ -70,7 +119,7 @@ app.post('/games/:gameId/cards', async (req, res) => {
 // POST can do the same thing, so leaving this out for now
 
 
-//DELETE card - HARD DELETE (for soft delete use PUT)
+//DELETE card - HARD DELETE (for soft delete use PUT or POST and set deleted attribute to true)
 app.delete('/cards/:cardId', async (req, res) => {
     let deletion = await db.collection('cards').doc(req.params.cardId).delete();
     res.json({'result': `deleted card: ${req.params.cardId}`});
