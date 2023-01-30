91691ec5053babf5ccd0415b90aef57ae0472366
Belthunder <chmball3@gmail.com>
2023-01-29 21:54:57 -0800

    Added more Handlers and Zones
    Added many remaining handlers, though they are mostly stubs at the moment. Added ZoneHandler, which UIHandler has used to make zones for the game!

--- client/src/helpers/UIHandler.js
+++ client/src/helpers/UIHandler.js
@@ -1,10 +1,38 @@
+import ZoneHandler from "./ZoneHandler";
+
 export default class UIHandler {
     constructor(scene) {
+
+        //the play area is going to be comprized of dropZones, which will be handled with a ZoneHandler.
+        this.zoneHandler = new ZoneHandler(scene);
+
+        this.buildZones = () => {
+            scene.dropZone = this.zoneHandler.renderZone(470, 500);
+            this.zoneHandler.renderOutline(scene.dropZone);
+        }
+
+        this.buildPlayerArea = () => {
+            scene.playerHandArea = scene.add.rectangle(470, 860, 850, 230);
+            scene.playerHandArea.setStrokeStyle(4, 0x0096FF);
+            scene.playerDeckArea = scene.add.rectangle(1000, 860, 155, 215);
+            scene.playerDeckArea.setStrokeStyle(3, 0x00FFFF);
+        }
+        
+        this.buildEnemyArea= () => {
+            scene.enemyArea = scene.add.rectangle(470, 135, 850, 230);
+            scene.enemyArea.setStrokeStyle(4, 0x880808);
+            scene.enemyArea = scene.add.rectangle(1000, 135, 155, 215);
+            scene.enemyArea.setStrokeStyle(3, 0x880808);
+        }
+
         this.buildGameText = () => {
             scene.dealCards = scene.add.text(960, 465, "Deal Cards").setFontSize(14).setFontFamily("Treubuchet MS");
         }
 
         this.buildUI = () => {
+            this.buildZones();
+            this.buildPlayerArea();
+            this.buildEnemyArea();
             this.buildGameText();
         }
 
--- client/src/scenes/game.js
+++ client/src/scenes/game.js
@@ -1,3 +1,8 @@
+import CardHandler from '../helpers/CardHandler';
+import DeckHandler from '../helpers/DeckHandler';
+import InteractiveHandler from '../helpers/InteractiveHandler';
+import GameHandler from '../helpers/GameHandler';
+import SocketHandler from '../helpers/SocketHandler';
 import UIHandler from "../helpers/UIHandler";
 
 export default class Game extends Phaser.Scene {
@@ -8,12 +13,19 @@ export default class Game extends Phaser.Scene {
     }
 
     preload() {
-
+        // we can preload provided assets. We will eventually want to hook this up to our database.
+        this.load.image('cyanCardBack', 'src/assets/CyanCardBack.png')
     }
 
     create() {
+        // Everything is going to be dealt with through various handlers.
+        this.CardHandler = new CardHandler();
+        this.DeckHandler = new DeckHandler(this);
+        this.GameHandler = new GameHandler(this);
+        this.SocketHandler = new SocketHandler(this);
         this.UIHandler = new UIHandler(this);
         this.UIHandler.buildUI();
+        this.InteractiveHandler = new InteractiveHandler(this);
     }
 
     update() {
