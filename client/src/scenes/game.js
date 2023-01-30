dcac7519f8f177a93de65a51426e029043df31dd
Belthunder <chmball3@gmail.com>
2023-01-29 18:26:20 -0800

    Created folders, game.js
    Created helpers and scenes folders, along with the scene game.js.
    Changed the index file to use the game.js scene rather than default.

---
+++ client/src/scenes/game.js
@@ -0,0 +1,20 @@
+export default class Game extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'Game'
+        })
+    }
+
+    preload() {
+
+    }
+
+    create() {
+
+    }
+
+    update() {
+        
+    }
+
+}
