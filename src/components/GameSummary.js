962ea8c7e4b3aa232621147452387337b188fd9c
Rasheednour <rasheednour@gmail.com>
2023-02-01 05:18:05 +0100

    Updated styling of game summary card

--- src/components/GameSummary.js
+++ src/components/GameSummary.js
@@ -1,3 +1,7 @@
+
+// This is the Game Summary card component, which is a box that shows a brief summary
+// about any given TCG to the user (title, description, creator, cover art, sample cards, play button)
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
@@ -8,7 +12,7 @@ function GameSummary({title, creator, description}) {
   return (
     <div className="GameSummary">
         <div className='cover-image'>
-            <img src={cover} alt='game cover logo'/>
+            <img src={cover} width="400" height="500" alt='game cover logo'/>
         </div>
 
         <div className='right-panel'>
