6b58f02dde5fc66ca3dde7475f2693038aa41ac8
Stew <weazle1@gmail.com>
2023-02-05 18:17:08 +0000

    added cors policy workaround

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const db = new Firestore();
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 8080;
+app.use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+      );
+      res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+      if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+      }
+      next();
+    });
 app.listen(port, () => {
   console.log(`Running on port: ${port}`);
 });
