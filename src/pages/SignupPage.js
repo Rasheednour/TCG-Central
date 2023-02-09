1bd50179b41092c080c9cd9d11aa6e82ef70b74d
Rasheednour <rasheednour@gmail.com>
2023-02-09 02:55:41 +0100

    adjusted pages styling

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import '../App.css';
+import './styles/SignUpPage.css'
 import SignUpForm from '../components/SignUpForm.js'
 import TopRibbon from '../components/TopRibbon.js'
 
 
 function SignUpPage() {
   return (
-    <>
+    <div className='SignUpPage'>
     <TopRibbon/>
     <SignUpForm/>
-    </>
+    </div>
   );
 }
 
