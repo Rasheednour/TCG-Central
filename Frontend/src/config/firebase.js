7dc193b9495cbb1ac19bf0d55de21c9f1036365d
Rasheednour <rasheednour@gmail.com>
2023-05-25 23:00:35 +0200

    updated firebase config file

--- Frontend/src/config/firebase.js
+++ Frontend/src/config/firebase.js
@@ -1,19 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDWAQvk3a-42R3dDy1IjbT5QqzCt9o0nhs",
-  authDomain: "tcg-maker-backend.firebaseapp.com",
-  projectId: "tcg-maker-backend",
-  storageBucket: "tcg-maker-backend.appspot.com",
-  messagingSenderId: "882921117330",
-  appId: "1:882921117330:web:01ca3e0743b2f38237f84b",
+  apiKey: "AIzaSyBUHVyDB-g5ovxYwqWy1ul5xykXnrJ2EVI",
+  authDomain: "tcg-central-backend.firebaseapp.com",
+  projectId: "tcg-central-backend",
+  storageBucket: "tcg-central-backend.appspot.com",
+  messagingSenderId: "31567552027",
+  appId: "1:31567552027:web:31b5bdf3bbd74f6471a2be",
+  measurementId: "G-6WLPJ8YJCB"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app);
+const analytics = getAnalytics(app);
