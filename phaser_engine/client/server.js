1b7b45b65a2d6dff2bb072f351be33ddb078c3df
Rasheednour <rasheednour@gmail.com>
2023-05-28 22:01:34 +0200

    fix favicon bug when app is deployed

--- phaser_engine/client/server.js
+++ phaser_engine/client/server.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require('express');
+const favicon = require('express-favicon');
 
 const app = express();
 const DIST_DIR = path.join(__dirname, '/src');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
+app.use(favicon(__dirname + '/src/favicon.ico'));
 app.use(express.static(DIST_DIR));
 app.get('*', (req, res) => {
   res.sendFile(HTML_FILE);
