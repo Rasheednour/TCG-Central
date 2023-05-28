835a49d9a312d055f47752914002f6cc372583e0
Rasheednour <rasheednour@gmail.com>
2023-05-28 15:57:50 +0200

    update phaser client socket url

--- phaser_engine/client/src/helpers/handlers/SocketHandler.js
+++ phaser_engine/client/src/helpers/handlers/SocketHandler.js
@@ -2,7 +2,7 @@ import io from "socket.io-client";
 
 export default class SocketHandler {
   constructor(scene) {
-    scene.socket = io('https://tcg-maker-phaser.herokuapp.com/');
+    scene.socket = io('https://tcg-phaser-client.herokuapp.com/');
     //scene.socket = io("http://localhost:3000");
     //server connects, and tells the server to deal a deck.
     scene.socket.on("connect", () => {
